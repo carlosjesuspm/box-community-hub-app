7a31fffffa0d2920b2a0a528cb9972364dac540d
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2026-05-26 08:15:30 +0200

    Capa Dominio ClassSession

--- src/main/java/ClassSession/domain/ClassSession.java
+++ src/main/java/ClassSession/domain/ClassSession.java
@@ -1,10 +1,34 @@
 package ClassSession.domain;
 
+import ClassSession.domain.vo.ClassSessionId;
+import lombok.Builder;
+import lombok.Getter;
 import user.domain.vo.UserId;
 
+/**
+ * Aggregate root representing a Class Session in the system.
+ *
+ * @since 1.0
+ */
+
+@Getter
+@Builder
 public class ClassSession {
 
-    private String classSessionId;
+    private final ClassSessionId classSessionId;
     private UserId coachId;
 
+    /**
+     * Creates a new {@code Box} with all its value objects.
+     *
+     * @param classSessionId   the unique identifier
+     * @param coachId          the box address
+     */
+    public ClassSession(
+            final ClassSessionId classSessionId,
+            final UserId coachId
+    ) {
+        this.classSessionId = classSessionId;
+        this.coachId = coachId;
+    }
 }
